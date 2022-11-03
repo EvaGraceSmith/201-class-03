cbaa2382d809c67bfb229b60eb5bda0fe00c3943
EvaGraceSmith <smithevagrace@gmail.com>
2022-11-02 20:57:10 -0500

    class notes

---
+++ js/demo.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// the user variable
+String, null, '' empty String
+If the user is an empty string or null, keep asking them for their name.If
+
+while (UserName === " " || user Name === null){
+    do some stuff
+}
+
+short cut 
+while (!userName){
+    userName = prompt('what is your name');
+}
+
+// Control flow of our if / else statements
+
+if(condition){
+
+} else {
+
+}
+
+if(condition){
+...
+} else if(second condition){
+
+    else{
+
+    }
+}
+    
+}
